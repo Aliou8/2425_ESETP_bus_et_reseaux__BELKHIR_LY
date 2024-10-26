37594ac82c1d253430b592ada3842a91f31fa077
Aliou8 <aliou.ly@ensea.fr>
2024-10-27 00:12:22 +0200

    commit initial

---
+++ Python_PI/SerialPI_STM32.py
@@ -0,0 +1,23 @@
+import serial
+
+ser = serial.Serial()
+
+def serialInit(baudrate , port):
+    ser.baudrate = baudrate
+    ser.port = port
+    ser.open()
+    if ser.is_open == True:
+        print("Serial init OK")
+    else:
+        print("Serial n'est pas bien initialiser")
+
+def serialWrite(message):
+    ser.write(message.encode())
+    ser.close()
+
+def serialRead(ser,size):
+    data = ser.read(size)
+    return data
+
+
+serialInit(115200,'COM4')
